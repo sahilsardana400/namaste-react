11918a0798c0da44e92a504f12804046bcc4f781
macrew_bitbucket <bitbucket@macrew.net>
2026-05-29 12:56:25 +0530

    episode 2:igniting app

--- App.js
+++ App.js
@@ -1,41 +1,5 @@
-// const heading = React.createElement('h1',{id:"heading",xyz:"sdsd"},'Content/children goes here');//this is how we create react element using react library
-
-//we will created nested div elements and will render in inside root dom element
-const parent = React.createElement(
-                   'div',
-                   {id:'parent'},
-                   [
-                    React.createElement('div',{id:'child1'}, [
-                        React.createElement('h1',{id:'heading'},'H1 tag'),
-                        React.createElement('h2',{id:'heading2'},'H2 tag')
-                    ]),
-                    React.createElement('div',{id:'child2'}, [
-                        React.createElement('h1',{id:'heading'},'H1 tag'),
-                        React.createElement('h2',{id:'heading2'},'H2 tag')
-                    ])
-                   ]
-                );
-
-
-
-//<div id="parent">
-
-    // <div id="subchild1">
-        // <div id="subchhild11">data11</div>
-        // <div id="subchhild12">data12</div>
-    // </div>
-        
-    // <div id="subchild2">
-        // <div id="subchhild21">data21</div>
-        // <div id="subchhild22">data22</div>
-    // </div>
-
-    // <div id="subchild3">
-        // <div id="subchhild31">data31</div>
-        // <div id="subchhild32">data32</div>
-    // </div>
-
-// </div>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const rootData = React.createElement(
                         'div',
@@ -45,17 +9,17 @@ const rootData = React.createElement(
                                 'div',
                                 {id:'subschild1'},
                                 [
-                                    React.createElement('div',{id:'subchhild11',class:'subchild'},'data11'),
-                                    React.createElement('div',{id:'subchhild12',class:'subchild2'},'data12')
+                                    React.createElement('div',{id:'subchhild11',className:'subchild'},'data11'),
+                                    React.createElement('div',{id:'subchhild12',className:'subchild2'},'data12')
                                 ]
                             ),
                             React.createElement('div',{id:'subschild2'},[
-                                React.createElement('div',{id:'subchhild21',class:'subchild'},'data21'),
-                                React.createElement('div',{id:'subchhild22',class:'subchild2'},'data22')
+                                React.createElement('div',{id:'subchhild21',className:'subchild'},'data21'),
+                                React.createElement('div',{id:'subchhild22',className:'subchild2'},'data22')
                             ]),
                             React.createElement('div',{id:'subschild3'},[
-                                React.createElement('div',{id:'subchhild31',class:'subchild'},'data31'),
-                                React.createElement('div',{id:'subchhild32',class:'subchild2'},'data32')
+                                React.createElement('div',{id:'subchhild31',className:'subchild'},'data31'),
+                                React.createElement('div',{id:'subchhild32',className:'subchild2'},'data32')
                             ])
                         ]
                         );
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-#Namaste React
+# Namaste React 
+
+# parcel
+  # local server
+  # dev build
+  # HMR = HOT module reloading/replacement
+  # File watching Algorihm written in C++
+  # caching faster builds
+  # Image optimization
+  # Minification
+  # Bundling
+  # Compress
+  # Consistent Hashing
+  # Code splitting
+  # Differential Bundling - support older browsers
+  # Diagnostic
+  # Error Handling
+  # Https
+  # Tree Shaking - Remove Unused Code
+  # https://parceljs.org/  Documentation
+  # Different dev and production bundles
+  # browsers list: To enable our app to fully work on browsers: https://browserslist.dev/
